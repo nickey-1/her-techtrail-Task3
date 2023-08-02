8a74b9d607ecc6f38e8c38be2c8ee7a552b549aa
nickey-1 <exquisitenickey@gmail.com>
2023-08-02 11:29:29 +0100

    just a commit

---
+++ photo gallery/script.js
@@ -0,0 +1,60 @@
+const change = document.getElementById("sunrise");
+change.addEventListener("click", (event)=>{
+    event.target.style.background = "brown";
+    event.target.style.color = "white";
+}
+);
+const changey = document.getElementById("beachy");
+changey.addEventListener("click", (event)=>{
+    event.target.style.background = "blue";
+    event.target.style.color = "gold";
+}
+);
+const change2 = document.getElementById("flowers");
+change2.addEventListener("click", (event)=>{
+    event.target.style.background = "grey";
+    event.target.style.color = "brown";
+}
+);
+const change3 = document.getElementById("island");
+change3.addEventListener("click", (event)=>{
+    event.target.style.background = "yellow";
+    event.target.style.color = "pink";
+}
+);
+const change4 = document.getElementById("sunset");
+change4.addEventListener("click", (event)=>{
+    event.target.style.background = "orange";
+    event.target.style.color = "pink";
+}
+);
+const change5 = document.getElementById("tower");
+change5.addEventListener("click", (event)=>{
+    event.target.style.background = "green";
+    event.target.style.color = "lilac";
+}
+);
+
+const changeyy = document.getElementById("nature");
+changeyy.addEventListener("click", (event)=>{
+    event.target.style.background = "indigo";
+    event.target.style.color = "black";
+}
+);
+const changeyy6 = document.getElementById("dreamcar");
+changeyy6.addEventListener("click", (event)=>{
+    event.target.style.background = "red";
+    event.target.style.color = "black";
+}
+);
+const changeyy7 = document.getElementById("flowerey");
+changeyy7.addEventListener("click", (event)=>{
+    event.target.style.background = "white";
+    event.target.style.color = "orange";
+}
+);
+
+
+
+
+
